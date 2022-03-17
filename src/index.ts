9fb555f0056ef46029b28c32e9b2204435b41f33
Mike Kotsur <miccots@gmail.com>
2022-03-17 14:40:03 +0100

    Some visual changes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ const blueText = document.currentScript?.getAttribute('data-blue-text')
 const yellowText = document.currentScript?.getAttribute('data-yellow-text')
 
 window.onload = () => {
-    console.log('BT', blueText)
     const template = createTemplate(
         blueText == null ? 'Stand with' : blueText,
         yellowText == null ? 'UKRAINE' : yellowText
